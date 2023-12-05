cd17b423a72cd5b0e137bb092aa83821311f654b
hanselmito <antonio@gmail.com>
2023-12-05 10:32:42 +0100

    model Finisher

--- Menu.java
+++ Menu.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Menu {
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+
+        boolean salir = false;
+        int opcion;  // Declarar la variable 'opcion' aquí
+
+        while (!salir) {  // Agregar un bucle while para repetir el menú hasta que 'salir' sea verdadero
+
+            System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            System.out.println("1. INICIAR PARTIDA ");
+            System.out.println("2. Salir");
+            System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-");
+            opcion = sn.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Este es el programa que realiza Funciones");
+                    break;
+                case 2:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println(" tu lees o comes bordillos a bocado ");
+            }
+        }
+    }
 }
